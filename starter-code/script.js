47dd349180a31870534f6829b5424802801f0cfa
afexEmma <105051557+afexEmma@users.noreply.github.com>
2022-05-18 11:22:38 +0000

    Update script.js
    Added Functionality for the pricing page

--- starter-code/script.js
+++ starter-code/script.js
@@ -25,4 +25,46 @@ menu.addEventListener('click', clickMenu);
 closes.addEventListener('click', clickClose);
     
     
+const month = document.querySelector(".month");
+const toggleBtn = document.querySelector(".toggle_btn");
+const ballOff = document.querySelector(".off_ball");
+const ballOn = document.querySelector(".on_ball");
+const year = document.querySelector(".year");
+const time = document.querySelectorAll(".time");
+const bPrice = document.querySelector('#b_p');
+const pPrice = document.querySelector('#p_p');
+const bnPrince = document.querySelector('#bn_p');
 
+
+function toggleOn(){
+    month.style.opacity = "0.5";
+    year.style.opacity = "1";
+    ballOff.style.display = 'none';
+    ballOn.style.display = 'block';
+    toggleBtn.style.backgroundColor = '#000000';
+    for(let i in time){
+        time[i].innerHTML = 'per year';
+    }
+    bPrice.innerHTML = '$190.00'
+    pPrice.innerHTML = '$390.00'
+    bnPrince.innerHTML = '$990.00'
+}
+
+function toggleOff(){
+    month.style.opacity = "1";
+    year.style.opacity = "0.5";
+    ballOff.style.display = 'block';
+    ballOn.style.display = 'none';
+    toggleBtn.style.backgroundColor = '#DFDFDF';
+    for(let i in time){
+        time[i].innerHTML = 'per month';
+    }
+    bPrice.innerHTML = '$19.00'
+    pPrice.innerHTML = '$39.00'
+    bnPrince.innerHTML = '$99.00'
+}
+
+
+
+ballOff.addEventListener('click', toggleOn)
+ballOn.addEventListener('click', toggleOff);
